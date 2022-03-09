6bea733aa2fe3404a087ed0147a5cfe946356e08
Risclover <85785443+Risclover@users.noreply.github.com>
2022-03-09 02:07:27 -0800

    Switched repositories

--- js files/script.js
+++ js files/script.js
@@ -28,3 +28,8 @@ function scrollFunction() {
     })
   }
 }
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
